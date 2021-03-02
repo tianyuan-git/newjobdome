4c336469e62b3f2bb91dc9c98c9758dccbd8dcb1
tianyuan-git <576092836@qq.com>
2021-03-03 04:57:29 +0800

    优化

--- src/components/Newjob.jsx
+++ src/components/Newjob.jsx
@@ -1,55 +1,28 @@
 import React from 'react'
-import { Table, Tooltip, message } from 'antd';
+import { Table, Tooltip, message, Spin } from 'antd';
 import EllipsisTooltip from './common/EllipsisTooltip';
 
 export default class Newjob extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            dataSource: [
-                {
-                    key: '1',
-                    job: '同城零售-ToB产品运营（猫超卡）-用户运营部门(同城零售-ToB产品运营（猫超卡）-用户运营部门)',
-                    address: '杭州',
-                    updateTime: '9分钟前',
-                },
-                {
-                    key: '2',
-                    job: '阿里云智能事业群-系统研发专家-机器学习平台',
-                    address: '杭州',
-                    updateTime: '19分钟前',
-                },
-                {
-                    key: '3',
-                    job: '中台运营事业部-品牌搜索产品运营-上海',
-                    address: '上海',
-                    updateTime: '19分钟前',
-                },
-                {
-                    key: '4',
-                    job: '阿里妈妈技术-前端开发专家/高级专家-北京',
-                    address: '北京',
-                    updateTime: '19分钟前',
-                },
-                {
-                    key: '5',
-                    job: '同城零售-渠道拓展专家-天猫超市',
-                    address: '杭州',
-                    updateTime: '19分钟前',
-                },
-            ]
+            loading: true
         }
     }
-    getNewData = () => {
-        let arr2 = [this.state.dataSource[0]]
-        let arr1 = this.state.dataSource.slice(1, 5)
+    getNewData = (dataSource) => {
+        let arr1 = [dataSource[0]]
+        //一次展示5条
+        let arr2 = dataSource.slice(1, 6)
+        let arr3 = dataSource.slice(6, dataSource.length)
         this.setState({
-            dataSource: [...arr1, ...arr2]
+            loading: false,
+            theDataSource: [...arr2, ...arr3, ...arr1],
+            newDataSource: [...arr2]
         });
     }
-    //创建定时器
-    carousel = () => {
-        this.setIntervalId = setInterval(this.getNewData, 1500);
+    // //创建定时器
+    carousel = (dataSource) => {
+        this.setIntervalId = setInterval(() => { this.getNewData(this.state.theDataSource ? this.state.theDataSource : dataSource) }, 1000);
     }
     //停止定时器
     stopCarousel = () => {
@@ -57,10 +30,73 @@ export default class Newjob extends React.Component {
     }
     //鼠标离开再次启动定时器
     goCarousel = () => {
-        this.carousel()
+        this.carousel(this.state.newDataSource)
     }
-    componentDidMount() {
-        this.carousel()
+    componentDidMount(carousel) {
+        //在次获取需要轮播的数据（调用后台接口）
+        const data = [
+            {
+                key: '1',
+                job: '同城零售-ToB产品运营（猫超卡）-用户运营部门(同城零售-ToB产品运营（猫超卡）-用户运营部门)',
+                address: '杭州',
+                updateTime: '9分钟前',
+            },
+            {
+                key: '2',
+                job: '阿里云智能事业群-系统研发专家-机器学习平台',
+                address: '杭州',
+                updateTime: '19分钟前',
+            },
+            {
+                key: '3',
+                job: '中台运营事业部-品牌搜索产品运营-上海',
+                address: '上海',
+                updateTime: '19分钟前',
+            },
+            {
+                key: '4',
+                job: '阿里妈妈技术-前端开发专家/高级专家-北京',
+                address: '北京',
+                updateTime: '19分钟前',
+            },
+            {
+                key: '5',
+                job: '同城零售-渠道拓展专家-天猫超市1',
+                address: '杭州',
+                updateTime: '19分钟前',
+            },
+            {
+                key: '6',
+                job: '同城零售-渠道拓展专家-天猫超市2',
+                address: '杭州',
+                updateTime: '19分钟前',
+            },
+            {
+                key: '7',
+                job: '同城零售-渠道拓展专家-天猫超市3',
+                address: '杭州',
+                updateTime: '19分钟前',
+            },
+            {
+                key: '8',
+                job: '同城零售-渠道拓展专家-天猫超市4',
+                address: '杭州',
+                updateTime: '19分钟前',
+            },
+            {
+                key: '9',
+                job: '同城零售-渠道拓展专家-天猫超市5',
+                address: '杭州',
+                updateTime: '19分钟前',
+            },
+            {
+                key: '10',
+                job: '同城零售-渠道拓展专家-天猫超市6',
+                address: '杭州',
+                updateTime: '19分钟前',
+            },
+        ];
+        this.carousel(data)
     }
     //组件销毁时清除定时器
     componentWillUnmount() {
@@ -130,9 +166,10 @@ export default class Newjob extends React.Component {
                 </div>
                 <Table
                     columns={columns}
-                    dataSource={this.state.dataSource}
+                    dataSource={this.state.newDataSource}
                     pagination={false}
                     showHeader={false}
+                    loading={this.state.loading}
                     onRow={record => {
                         return {
                             onMouseEnter: event => { this.stopCarousel() },
